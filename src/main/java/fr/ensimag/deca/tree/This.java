66fd2c683c999489cd5f81fc07f3086c484197ae
Ibrahim LEMDAGHRI ALAOUI <ibrahim.lemdaghri-alaoui@grenoble-inp.org>
2023-01-21 15:07:35 +0100

    ajout depuis bentouy-classA et ajout de decompile à revoir le boolean impl

---
+++ src/main/java/fr/ensimag/deca/tree/This.java
@@ -0,0 +1,50 @@
+package fr.ensimag.deca.tree;
+
+import java.io.PrintStream;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+
+public class This extends AbstractExpr {
+	
+
+	@Override
+	public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
+			throws ContextualError {
+		// TODO Auto-generated method stub
+		if (currentClass == null) {
+			throw new ContextualError("impossible d'appeler this dans main", this.getLocation());
+		}
+		this.setType(currentClass.getType());
+		return this.getType();
+	}
+
+	@Override
+	public void decompile(IndentPrintStream s) {
+		// TODO Auto-generated method stub
+		s.print("this");
+	}
+
+	@Override
+	protected void prettyPrintChildren(PrintStream s, String prefix) {
+        // leaf node => nothing to do
+		
+	}
+
+	@Override
+	protected void iterChildren(TreeFunction f) {
+        // leaf node => nothing to do
+
+	}
+	
+    @Override
+    String prettyPrintNode() {
+        return "this ";
+    }
+
+}
+
